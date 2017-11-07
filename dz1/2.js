b8ac8c030564306d16a22a05981eb49ddbb6ebe1
Rozzel <rozzel@mail.ru>
2017-11-07 14:15:11 +0300

    dz1

---
+++ dz1/2.js
@@ -0,0 +1,24 @@
+var test;              // объявляем переменную
+console.log(test);
+
+
+test = 40;             // присваиваем одно значение
+console.log(test);
+
+
+test = 'сорок грамм';  // присваиваем совершенно другое значение
+console.log(test);
+
+var test = 50 + (80 - 2) * 120 / 12;
+console.log('50 + (80 - 2) * 120 / 12 = ' + test);
+
+var test = 5.52*3.95;
+console.log(test);
+
+var test = 6%7;
+console.log(test);
+
+var test = 7%7;
+console.log('Остаток от деления 7 на 7: ' + test);
+
+// Работа с переменными.
---
+++ dz1/3.js
@@ -0,0 +1,10 @@
+var admin;
+var name = 'Василий';
+
+admin = name;
+
+console.log(admin)
+
+/*
+Объявить две переменные: admin и name. Записать в name строку "Василий"; Скопировать значение из name в admin. Вывести admin (должно вывести «Василий»).
+*/
