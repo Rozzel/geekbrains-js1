5c8571598e7d5d4234af2570c01c2fc91bc91298
Vladislav Golik <Vladislav.Golik@gmail.com>
2017-11-17 15:05:04 +0300

    Правки шифратора

--- dz4/praktika/shifr.js
+++ dz4/praktika/shifr.js
@@ -17,5 +17,8 @@ for(var i = 0; i < encodedMessage.length; i++){
 
 caesarsCodeDecoded('8 28 36 52 56 40 23 31 56 39 38 28 48 52 58 56 38 27 32 37 56 40 23 31 56 39 38 41 39 32 57');
 
+/*
 
+caesarsCodeEncoded('Семь раз поешь, один раз поспи.');
 
+*/
