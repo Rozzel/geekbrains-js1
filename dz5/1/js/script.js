cc976c88802f2f448d32b9a3aacf2839f1c22af4
Vladislav Golik <Vladislav.Golik@gmail.com>
2017-11-19 17:31:44 +0300

    Рефакторинг кода

--- dz5/1/js/script.js
+++ dz5/1/js/script.js
@@ -27,64 +27,32 @@ for (var i = 0; i < 100 ; i++) {
                 i === 80
             ) {
                 outlineleftNum();
-            }else if (  i === 19||
-                        i === 29||
-                        i === 39||
-                        i === 49||
-                        i === 59||
-                        i === 69||
-                        i === 79||
-                        i === 89
+        }else if (  i === 19||
+                    i === 29||
+                    i === 39||
+                    i === 49||
+                    i === 59||
+                    i === 69||
+                    i === 79||
+                    i === 89
             ){
                 outlineRightNum();
             }else if (  i < 21){
-                if (i % 2 === 0){
-                    black();
-                }else {
-                    white();
-                }
+                chetno();
             }else if ( i < 31) {
-                if (i % 2 !== 0){
-                    black();
-                }else {
-                    white();
-                }
+                nechetno();
             }else if ( i < 41){
-                if (i % 2 === 0){
-                    black();
-                }else {
-                    white();
-                }
+                chetno();
             }else if(i < 51){
-                if (i % 2 !== 0){
-                    black();
-                }else {
-                    white();
-                }
+                nechetno();
             }else if(i < 61){
-            if (i % 2 === 0){
-                black();
+                chetno();
+            }else if (i < 71){
+                nechetno();
+            }else if (i < 81){
+                chetno();
             }else {
-                white();
-            }
-        }else if (i < 71){
-            if (i % 2 !== 0){
-                black();
-            }else {
-                white();
-            }
-        }else if (i < 81){
-                if (i % 2 === 0){
-                    black();
-                }else {
-                    white();
-                }
-            }else {
-                if (i % 2 !== 0){
-                    black();
-                }else {
-                    white();
-                }
+                nechetno();
             }
     } else {
         outlineBot();
@@ -97,38 +65,43 @@ function black() {
     divBoardSel.appendChild(divSquare);
     divSquare.innerText = keysBoard[i];
 }
-
 function white() {
         divSquare.className = 'square square-white';
         divBoardSel.appendChild(divSquare);
         divSquare.innerText = keysBoard[i];
-    }
-
+}
+function outlineTop(){
+        divSquare.className = 'square square-no-outline rotate';
+        divBoardSel.appendChild(divSquare);
+        divSquare.innerText = keys[i];
+}
 function outlineBot(){
     divSquare.className = 'square square-no-outline';
     divBoardSel.appendChild(divSquare);
     divSquare.innerText = keys[i - 90];
 }
-
-    function outlineTop(){
-        divSquare.className = 'square square-no-outline rotate';
-        divBoardSel.appendChild(divSquare);
-        divSquare.innerText = keys[i];
-    }
-
-function   outlineleftNum(){
+function outlineleftNum(){
     divSquare.className = 'square square-no-outline';
     divBoardSel.appendChild(divSquare);
 divSquare.innerText = leftNum--;
 }
-
 function outlineRightNum(){
     divSquare.className = 'square square-no-outline rotate';
     divBoardSel.appendChild(divSquare);
     divSquare.innerText = RightNum--;
 }
-
-
-
+function chetno() {
+    if (i % 2 === 0){
+        black();
+    }else {
+        white();
+    }
+}
+function nechetno() {
+    if (i % 2 !== 0){
+        black();
+    }else {
+        white();
+    }
+}
 };
-
