bc1b955032a2888c2dffc5c9e875cd295a843ac0
Vladislav Golik <Vladislav.Golik@gmail.com>
2017-11-20 10:58:17 +0300

    Рефакторинг кода

--- dz5/1/js/script.js
+++ dz5/1/js/script.js
@@ -1,117 +1,123 @@
 window.onload = function () {
 
+generateBoard();
 
-var divBoard = document.createElement('div');
-divBoard.className = 'board';
-document.body.appendChild(divBoard);
-var keys = ' ABCDEFGH '.split('');
-var keysBoard = '           чЛадья чКонь чСлон чФерзь чКороль чСлон чКонь чЛадья   чПешка чПешка чПешка чПешка чПешка чПешка чПешка чПешка   A6 B6 C6 D6 E6 F6 G6 H6   A5 B5 C5 D5 E5 F5 G5 H5   A4 B4 C4 D4 E4 F4 G4 H4   A3 B3 C3 D3 E3 F3 G3 H3   бПешка бПешка бПешка бПешка бПешка бПешка бПешка бПешка   бЛадья бКонь бСлон бФерзь бКороль бСлон бКонь бЛадья'.split(' ');
-window.leftNum = 8;
-window.RightNum = 8;
+function generateBoard() {
+    var divBoard = document.createElement('div');
+    divBoard.className = 'board';
+    document.body.appendChild(divBoard);
+    var keys = 'ABCDEFGH'.split('');
+    var keysBoard = 'Ладья Конь Слон Ферзь Король Слон Конь Ладья Пешка б ч'.split(' ');
+    window.leftNum = 8;
+    window.RightNum = 8;
 
-for (var i = 0; i < 100 ; i++) {
-    var divBoardSel = document.querySelector('.board');
-    var divSquare = document.createElement('div');
+    for (var i = 0; i < 100; i++) {
+        var divBoardSel = document.querySelector('.board');
+        var divSquare = document.createElement('div');
 
-    if (i < 10){
+        if (i < 10 ) {
             outlineTop();
-
         } else if (i < 90) {
-            if (i === 10||
-                i === 20||
-                i === 30||
-                i === 40||
-                i === 50||
-                i === 60||
-                i === 70||
+            if (i === 10 ||
+                i === 20 ||
+                i === 30 ||
+                i === 40 ||
+                i === 50 ||
+                i === 60 ||
+                i === 70 ||
                 i === 80
             ) {
                 outlineleftNum();
-        }else if (  i === 19||
-                    i === 29||
-                    i === 39||
-                    i === 49||
-                    i === 59||
-                    i === 69||
-                    i === 79||
-                    i === 89
-            ){
+            } else if (i === 19 ||
+                i === 29 ||
+                i === 39 ||
+                i === 49 ||
+                i === 59 ||
+                i === 69 ||
+                i === 79 ||
+                i === 89
+            ) {
                 outlineRightNum();
-            }else if (i < 21){
+            } else if (i < 21) {
                 chetno();
                 fiGure();
-            }else if (i < 31) {
+            } else if (i < 31) {
                 nechetno();
                 fiGure();
-            }else if (i < 41){
+            } else if (i < 41) {
                 chetno();
-            }else if(i < 51){
+            } else if (i < 51) {
                 nechetno();
-            }else if(i < 61){
+            } else if (i < 61) {
                 chetno();
-            }else if (i < 71){
+            } else if (i < 71) {
                 nechetno();
-            }else if (i < 81){
+            } else if (i < 81) {
                 chetno();
                 fiGure();
-            }else {
+            } else {
                 nechetno();
                 fiGure();
-
             }
-    } else {
-        outlineBot();
+        } else {
+            outlineBot();
+        }
     }
-
-}
-function fiGure() {
-    divSquare.innerText = keysBoard[i];
-}
-function black() {
-    divSquare.className = 'square square-black';
-    divBoardSel.appendChild(divSquare);
-}
-function white() {
+    function fiGure() {
+      if (i < 19){
+          divSquare.innerText = keysBoard[10] + keysBoard[i - 11];
+        } else if (i < 29){
+          divSquare.innerText = keysBoard[10] + keysBoard[8];
+        } else if (i < 79){
+          divSquare.innerText = keysBoard[9] + keysBoard[8];
+      } else{
+          divSquare.innerText = keysBoard[9] + keysBoard[i - 81];
+      }
+    }
+    function black() {
+        divSquare.className = 'square square-black';
+        divBoardSel.appendChild(divSquare);
+    }
+    function white() {
         divSquare.className = 'square square-white';
         divBoardSel.appendChild(divSquare);
-}
-function outlineTop(){
+    }
+    function outlineTop() {
         divSquare.className = 'square square-no-outline rotate';
         divBoardSel.appendChild(divSquare);
-        divSquare.innerText = keys[i];
-}
-function outlineBot(){
-    divSquare.className = 'square square-no-outline';
-    divBoardSel.appendChild(divSquare);
-    divSquare.innerText = keys[i - 90];
-}
-function outlineleftNum(){
-    divSquare.className = 'square square-no-outline';
-    divBoardSel.appendChild(divSquare);
-divSquare.innerText = leftNum--;
-}
-function outlineRightNum(){
-    divSquare.className = 'square square-no-outline rotate';
-    divBoardSel.appendChild(divSquare);
-    divSquare.innerText = RightNum--;
-}
-function chetno() {
-    if (i % 2 === 0){
-        black();
-    }else {
-        white();
+        if ( i > 0 && i < 9){
+            divSquare.innerText = keys[i - 1];
+        }
     }
-}
-function nechetno() {
-    if (i % 2 !== 0){
-        black();
-    }else {
-        white();
+    function outlineBot() {
+        divSquare.className = 'square square-no-outline';
+        divBoardSel.appendChild(divSquare);
+        if ( i > 90 && i < 99){
+            divSquare.innerText = keys[i - 91];
+        }
     }
-}
-};
-
-
-/* Кладбище идей
-var keysBoard = '           A8 B8 C8 D8 E8 F8 G8 H8   A7 B7 C7 D7 E7 F7 G7 H7   A6 B6 C6 D6 E6 F6 G6 H6   A5 B5 C5 D5 E5 F5 G5 H5   A4 B4 C4 D4 E4 F4 G4 H4   A3 B3 C3 D3 E3 F3 G3 H3   A2 B2 C2 D2 E2 F2 G2 H2   A1 B1 C1 D1 E1 F1 G1 H1'.split(' ');
- */
+    function outlineleftNum() {
+        divSquare.className = 'square square-no-outline';
+        divBoardSel.appendChild(divSquare);
+        divSquare.innerText = leftNum--;
+    }
+    function outlineRightNum() {
+        divSquare.className = 'square square-no-outline rotate';
+        divBoardSel.appendChild(divSquare);
+        divSquare.innerText = RightNum--;
+    }
+    function chetno() {
+        if (i % 2 === 0) {
+            black();
+        } else {
+            white();
+        }
+    }
+    function nechetno() {
+        if (i % 2 !== 0) {
+            black();
+        } else {
+            white();
+        }
+    }
+}};
