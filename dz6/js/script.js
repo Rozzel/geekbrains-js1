315e52870993b6add4b165a5e90777e3cea0cb9d
Vladislav Golik <Vladislav.Golik@gmail.com>
2017-11-23 17:03:24 +0300

    Закончил функцию очистки корзины

--- dz6/js/script.js
+++ dz6/js/script.js
@@ -1,11 +1,12 @@
 window.onload = function () {
     init();
+    cleanBasketInit();
     basketInit();
 };
 
 
 function init(){
-    var images = document.getElementsByTagName("img");
+    var images = document.getElementsByClassName("galleryImg");
     for (var i = 0; i < images.length; i++) {
         images[i].onclick = changeBigPicture;
     }
@@ -22,21 +23,41 @@ function changeBigPicture(event){
 
 }
 
+
 function basketInit() {
-    var a = document.getElementsByTagName("a");
+    var a = document.getElementsByClassName("add-button");
     for (var i = 0; i < a.length; i++){
-        a[i].onclick = event.preventDefault(basketPicture);
+        a[i].onclick = basketPicture;
     }
-
-
 }
-
-
-function basketPicture() {
+function  basketPicture() {
     var basketBall = document.getElementById("basketBall");
-    basketBall.innerHTML = "<p>Карзина пуста</p>";
+
+    var basketTovar = document.createElement('div');
+        basketTovar.className = 'basketTovar';
+        basketTovar.textContent = 'test';
+
+    basketBall.appendChild(basketTovar);
+    event.preventDefault();
+
+
     //var smallSrc = event.target.getAttribute('src');
     //var newImg = document.createElement('img');
     //newImg.src = smallSrc.replace('small', 'big');
     //basketBall.appendChild(newImg);
+}
+
+
+function cleanBasketInit() {
+    var a = document.getElementById("cleanBasket");
+    a.onclick = cleanBasket;
+}
+function cleanBasket() {
+    var basketBall = document.getElementById("basketBall");
+    basketBall.innerHTML = "<p>Карзина пуста</p>";
+    var sumNo = document.getElementById("sumNo");
+    sumNo.innerHTML = "0";
+    var sumRub = document.getElementById("sumRub");
+    sumRub.innerHTML = "0";
+    event.preventDefault();
 }
