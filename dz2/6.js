def6641c1ac5939c2cc32cd64ef642ec14b84ef1
Vladislav <rozzel@mail.ru>
2019-01-15 14:10:16 +0300

    удолил brake он не нужен

--- dz2/6.js
+++ dz2/6.js
@@ -2,21 +2,15 @@ function mathOperation(arg1, arg2, operation){
     switch(operation){
         case '+':
             return arg1 + arg2;
-            break;
         case '-':
             return arg1 - arg2;
-            break;
         case '/':
             return arg1 / arg2;
-            break;
         case '*':
             return arg1 * arg2;
-            break;
         case '%':
             return arg1 % arg2;
-            break;
-    }
+    };
 }
 
 console.log(mathOperation(4, 6, '+'));
-
